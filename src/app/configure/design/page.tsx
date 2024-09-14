900b7e8c654fb24bcbf79b72e8874e09a96886ed
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-09-14 04:39:00 +0100

    installation error with sharp

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,11 @@
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const { id } = searchParams;
+
+  return;
+}
