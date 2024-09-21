688aedaf25e67bd35cb2488e3c2a38854b73e62c
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-09-21 13:03:11 +0100

    creating auth call back

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -10,17 +10,17 @@ import { useMutation } from "@tanstack/react-query";
 import { ArrowRight, Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
-// import { createCheckoutSession } from "./actions";
-// import { useRouter } from "next/navigation";
-// import { useToast } from "@/components/ui/use-toast";
-// import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-// import LoginModal from "@/components/LoginModal";
+import { createCheckoutSession } from "./actions";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import LoginModal from "@/components/LoginModal";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
-  //   const router = useRouter();
-  //   const { toast } = useToast();
-  //   const { id } = configuration;
-  //   const { user } = useKindeBrowserClient();
+  const router = useRouter();
+  const { toast } = useToast();
+  const { id } = configuration;
+  const { user } = useKindeBrowserClient();
   const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
 
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
@@ -41,32 +41,32 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     totalPrice += PRODUCT_PRICES.material.polycarbonate;
   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
 
-  //   const { mutate: createPaymentSession } = useMutation({
-  //     mutationKey: ["get-checkout-session"],
-  //     mutationFn: createCheckoutSession,
-  //     onSuccess: ({ url }) => {
-  //       if (url) router.push(url);
-  //       else throw new Error("Unable to retrieve payment URL.");
-  //     },
-  //     onError: () => {
-  //       toast({
-  //         title: "Something went wrong",
-  //         description: "There was an error on our end. Please try again.",
-  //         variant: "destructive",
-  //       });
-  //     },
-  //   });
+  const { mutate: createPaymentSession } = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+      else throw new Error("Unable to retrieve payment URL.");
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again.",
+        variant: "destructive",
+      });
+    },
+  });
 
-  //   const handleCheckout = () => {
-  //     if (user) {
-  //       // create payment session
-  //       createPaymentSession({ configId: id });
-  //     } else {
-  //       // need to log in
-  //       localStorage.setItem("configurationId", id);
-  //       setIsLoginModalOpen(true);
-  //     }
-  //   };
+  const handleCheckout = () => {
+    if (user) {
+      // create payment session
+      createPaymentSession({ configId: id });
+    } else {
+      // need to log in
+      localStorage.setItem("configurationId", id);
+      setIsLoginModalOpen(true);
+    }
+  };
 
   return (
     <>
@@ -80,7 +80,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
         />
       </div>
 
-      {/* <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} /> */}
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
 
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
@@ -161,7 +161,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
             <div className="mt-8 flex justify-end pb-12">
               <Button
-                // onClick={() => handleCheckout()}
+                onClick={() => handleCheckout()}
                 className="px-4 sm:px-6 lg:px-8"
               >
                 Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
