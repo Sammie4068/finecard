2ab00765ebd3a9f241470b662cf2fe78fc3aa5dd
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-09-26 06:59:46 +0100

    change currency to ngn

--- src/config/products.ts
+++ src/config/products.ts
@@ -1,11 +1,11 @@
 export const PRODUCT_PRICES = {
   finish: {
     default: 0,
-    silk: 3_00,
-    coarse: 3_00,
-    gloss: 5_00,
-    foil: 10_00,
+    silk: 1000_00,
+    coarse: 1000_00,
+    gloss: 2000_00,
+    foil: 2500_00,
   },
 } as const;
 
-export const BASE_PRICE = 15_00;
+export const BASE_PRICE = 5000_00;
