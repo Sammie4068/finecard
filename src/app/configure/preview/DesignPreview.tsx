940f462ccf05b5907e70e57df58c949ac5b5f77f
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-09-30 10:15:04 +0100

    Tired af

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -38,8 +38,6 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     mutationKey: ["get-checkout-session"],
     mutationFn: createCheckoutSession,
     onSuccess: ({ url, status }) => {
-      console.log(url, status);
-
       if (status) {
         localStorage.setItem("configurationId", id);
         setIsLoginModalOpen(true);
